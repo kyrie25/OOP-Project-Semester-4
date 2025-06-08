ba2b0183a1e2c55b6aae0fd307c8437b04d91767
JakeConal <23120124@student.hcmus.edu.vn>
2025-06-08 20:19:33 +0700

    feat: mark TriTri's task

--- src/ApplicationController.h
+++ src/ApplicationController.h
@@ -15,7 +15,7 @@ private:
 
     void handleCustomerMenu(User* user); //view shop, buy product, manage payment methods
     void handleShopMenu(User* user); //view products, buy products
-    void handlePaymentMethod(User* user); //add or remove payment methods
+    void handlePaymentMethod(User* user); //add or remove payment methods, (Xuan tri's task)
 
     void handleSellerMenu(User* user); //view products, add or remove products, view orders
     void handleProduct(User* user); //add or remove products
